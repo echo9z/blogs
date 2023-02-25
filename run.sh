731899ee5740e85f941403ed01a60172a572dbd0
echo9z <1131223522@qq.com>
2023-02-25 15:25:09 +0800

    ci: :construction_worker: 添加ci/cd,进行测试

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# 定义应用组名
+group_name='blog'
+# 定义应用名称
+app_name='nestjs_blogs'
+# 定义应用版本
+app_version='1.0.0'
+# 定义应用环境
+profile_active='prod'
+echo '----copy jar----'
+docker stop ${app_name}
+echo '----stop container----'
+docker rm ${app_name}
+echo '----rm container----'
+docker rmi ${group_name}/${app_name}:${app_version}
+echo '----rm image----'
+# 打包编译docker镜像
+docker build -t ${group_name}/${app_name}:${app_version} .
+echo '----build image----'
+
+docker run -id --name ${app_name} \
+-e TZ="Asia/Shanghai" \
+-p 18080:18080 \
+--net blogs_nework \
+-v /root/blogs/assets:/app/assets \
+${group_name}/${app_name}:${app_version}
+echo '----start container----'
+
